c96d4f11628405c4251de91f387e969b39ced063
Stoyan Dimov <s.dimov@codelogic.eu>
2016-05-05 17:28:46 +0200

    Add SendGrid as an email delivery service

--- src/SchoDotCom.WebUI/Models/EmailService.cs
+++ src/SchoDotCom.WebUI/Models/EmailService.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNet.Identity;
-using System;
+using SendGrid;
+using System.Configuration;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace SchoDotCom.WebUI.Models
@@ -8,8 +10,26 @@ namespace SchoDotCom.WebUI.Models
     {
         public Task SendAsync(IdentityMessage message)
         {
-            // Plug in your email service here to send an email.
-            throw new NotImplementedException();
+            // Create the email object first, then add the properties.
+            var sendGridMessage = new SendGridMessage()
+            {
+                From = new MailAddress("john@example.com", "John Smith"),
+                Subject = message.Subject,
+                Text = message.Body
+            };
+            sendGridMessage.AddTo(message.Destination);
+
+            return SendAsync(sendGridMessage);
+        }
+
+        public Task SendAsync(SendGridMessage message)
+        {
+            var apiKey = ConfigurationManager.AppSettings["sendgrid:ApiKey"] as string;
+
+            // Create an Web transport for sending email.
+            var transportWeb = new Web(apiKey);
+
+            return transportWeb.DeliverAsync(message);
         }
     }
 }
