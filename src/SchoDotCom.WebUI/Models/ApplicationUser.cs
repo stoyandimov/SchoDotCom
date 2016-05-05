ac5cac5dc6e6e7b75109a2b8efa69285f3ffe585
Stoyan Dimov <s.dimov@codelogic.eu>
2016-05-05 16:27:09 +0200

    Split application user models into their own files

--- src/SchoDotCom.WebUI/Models/ApplicationUser.cs
+++ src/SchoDotCom.WebUI/Models/ApplicationUser.cs
@@ -5,7 +5,8 @@ using System.Threading.Tasks;
 
 namespace SchoDotCom.WebUI.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, 
+    // please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
@@ -14,19 +15,6 @@ namespace SchoDotCom.WebUI.Models
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
             return userIdentity;
-        }
-    }
-
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole, string, IdentityUserLogin, IdentityUserRole, IdentityUserClaim>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection")
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
         }
     }
 }
