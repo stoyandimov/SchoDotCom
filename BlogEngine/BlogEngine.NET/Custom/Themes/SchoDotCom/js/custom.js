0c4d729ab132887c7dac1bada9a0832636d69a63
Stoyan Dimov <s.dimov@codelogic.eu>
2016-06-15 20:23:36 +0200

    Hide tag cloud if less than 3 tags

---
+++ BlogEngine/BlogEngine.NET/Custom/Themes/SchoDotCom/js/custom.js
@@ -0,0 +1,19 @@
+﻿// Tag cloud
+$(function () {
+    try {
+        TagCanvas.Start('tagCanvas', 'tags', {
+            initial: [-0.1, -0.05],
+            textColour: '#fff',
+            textFont: 'Segoe UI',
+            outlineColour: '#fff',
+            outlineMethod: 'none',
+            reverse: true,
+            depth: 0.8,
+            maxSpeed: 0.05
+        });
+        // more options at http://www.goat1000.com/tagcanvas-options.php
+    } catch (e) {
+        // something went wrong, hide the canvas container
+        document.getElementById('tagCanvasContainer').style.display = 'none';
+    }
+});
